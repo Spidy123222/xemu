0d73cca0e0c77b52a59994e495e8ffb4408249dd
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2023-06-29 17:09:16 -0700

    Update compat.cc

--- ui/xui/compat.cc
+++ ui/xui/compat.cc
@@ -206,7 +206,7 @@ void CompatibilityReporter::Draw()
     ImGui::SetCursorPosX(ImGui::GetWindowWidth()-(120+10)*g_viewport_mgr.m_scale);
 
     ImGui::SetItemDefaultFocus();
-    if (!g_config.perf.override_clockspeed)
+    if (g_config.perf.override_clockspeed)
     {
         ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
         ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
@@ -218,8 +218,9 @@ void CompatibilityReporter::Draw()
             is_open = false;
         }
     }
-    if (!g_config.perf.override_clockspeed)
+    if (g_config.perf.override_clockspeed)
     {
+        ImGui::Text("Reports using Emulated CPU clock override is not allowed.");
         ImGui::PopItemFlag();
         ImGui::PopStyleVar();
     }
