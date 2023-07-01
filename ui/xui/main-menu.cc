99a7057379d9c112129f0d2d91ad90d6e785fb92
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2023-06-30 23:47:24 -0700

    Update main-menu.cc

--- ui/xui/main-menu.cc
+++ ui/xui/main-menu.cc
@@ -67,7 +67,7 @@ void MainMenuGeneralView::Draw()
     
     char buf[32];
 
-    if (g_config.perf.cpu_clockspeed >= 0.505|| g_config.perf.cpu_clockspeed <= 0.495) {g_config.perf.cpu_clockspeed = 0.5;}
+    if (g_config.perf.cpu_clockspeed > 0.505 || g_config.perf.cpu_clockspeed < 0.495) {g_config.perf.cpu_clockspeed = 0.5;}
     
     snprintf(buf, sizeof(buf), "Clock Speed %d%% (%.2f MHz)",
              (int)(g_config.perf.cpu_clockspeed * 200), (733333333.0 * g_config.perf.cpu_clockspeed * 2.0)/1000000);
