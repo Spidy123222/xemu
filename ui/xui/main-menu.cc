9489a3e0fa95dfb91f0b0bb7fb03c553efcb0782
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2023-07-22 02:18:31 -0700

    Update main-menu.cc

--- ui/xui/main-menu.cc
+++ ui/xui/main-menu.cc
@@ -69,7 +69,7 @@ void MainMenuGeneralView::Draw()
     snprintf(buf, sizeof(buf), "Clock Speed %d%% (%.2f MHz)", (int)(g_config.perf.cpu_clockspeed * 100), (733333333 * g_config.perf.cpu_clockspeed)/1000000);
     Slider("Virtual CPU clock", &g_config.perf.cpu_clockspeed, buf, 0.01f, 2.f, 0.01f);
 
-    if (std::fabs(g_config.perf.cpu_clockspeed - 1.f) <= 0.009f) {
+    if (fabs(g_config.perf.cpu_clockspeed - 1.f) <= 0.009f) {
         g_config.perf.cpu_clockspeed = 1;
     }
     
